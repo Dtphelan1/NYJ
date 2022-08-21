e0ab4a78622cfea9248cfd32ea12d76b1865c3e7
Dylan Phelan <dtphelan1@gmail.com>
2022-08-21 18:13:07 -0400

    remove old images; compress main inkblot; add fade to main

--- fade-in.js
+++ fade-in.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $(".fade-1").addClass("load");
-    $(".fade-2").addClass("load");
-    $(".fade-3").addClass("load");
+  $(".fade-0").addClass("load");
+  $(".fade-1").addClass("load");
+  $(".fade-2").addClass("load");
+  $(".fade-3").addClass("load");
 });
